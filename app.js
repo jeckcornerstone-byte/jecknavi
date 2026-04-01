e3eadcd5c5fd8ec077267a2cf2d323c0c3380a91
jeckcornerstone-byte <jeck.cornerstone@gmail.com>
2026-04-01 14:43:13 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-let selectedDestination="ICU"; 
-window.onload=()=
-document.querySelector("a-marker").addEventListener("markerFound",()=
-let arrow=document.getElementById("arrow"); 
-arrow.innerHTML='<a-cone color="green" height="0.7"></a-cone>'; 
-arrow.setAttribute("rotation","0 45 0"); 
-}); 
-}; 
